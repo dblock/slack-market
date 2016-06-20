749f706b41ea65cffb6b52cd1811464a47dca3a7
dblock <dblock@dblock.org>
2016-06-19 22:01:56 -0400

    Updated app name and free service location.

--- slack-market/info.rb
+++ slack-market/info.rb
@@ -1,11 +1,11 @@
 module SlackMarket
   INFO = <<-EOS
-BotServer #{SlackMarket::VERSION}
+Slack Market #{SlackMarket::VERSION}
 
 © 2016 Daniel Doubrovkine & Contributors, MIT License
 https://twitter.com/dblockdotorg
 
-Free Service at http://slack-market.herokuapp.com
+Free Service at https://market.playplay.io
 Open-Source at https://github.com/dblock/slack-market
   EOS
 end

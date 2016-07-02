d648b1af975acf81cdc139d0ab3984d75e383795
David Druker <ddruker@users.noreply.github.com>
2016-07-02 09:39:20 -0400

    Changed slack_endpoint to be called graph_endpoint

--- slack-market/api/endpoints/graph_endpoint.rb
+++ slack-market/api/endpoints/graph_endpoint.rb
@@ -12,10 +12,6 @@ module Api
         end
 
         post do
-          p params.class
-          # slack interactive messages send a payload.
-          # the code below formats that object into a slack message
-          # response to the channel
           payload = JSON.parse(params[:payload])
           button_name = payload['actions'][0]['name']
           button_value = payload['actions'][0]['value']
@@ -27,20 +23,17 @@ module Api
           quotes = Market.quotes([stock_symbol])
 
           slack_attachment = Market.to_slack_attachment(quotes[0], charts: chart, button: button_name)
-          # verifying message token
-          if token == ENV['SLACK_VERIFICATION_TOKEN']
-            # formatted Slack message response
-            {
-              as_user: true,
-              channel: channel,
-              ts: ts,
-              token: token,
-              text: "Below is the #{button_name} chart",
-              attachments: [slack_attachment]
-            }
-          else
-            error! 'Message token is not coming from Slack.', 401 unless token == ENV['SLACK_VERIFICATION_TOKEN']
-          end
+
+          error! 'Message token is not coming from Slack.', 401 unless token == ENV['SLACK_VERIFICATION_TOKEN']
+          # formatted Slack message response
+          {
+            as_user: true,
+            channel: channel,
+            ts: ts,
+            token: token,
+            text: "Below is the #{button_name} chart",
+            attachments: [slack_attachment]
+          }
         end
       end
     end

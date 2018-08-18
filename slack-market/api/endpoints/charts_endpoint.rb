767d1a0c2ef3b0881462761a42672440ec1ce49f
dblock <dblock@dblock.org>
2018-08-18 15:38:02 -0400

    Fix charts, use IEX.

--- slack-market/api/endpoints/charts_endpoint.rb
+++ slack-market/api/endpoints/charts_endpoint.rb
@@ -20,23 +20,12 @@ module Api
                    else '1d'
           end
 
-          interval = period == '1d' ? 360 : 86_400
-
-          url = "https://finance.google.com/finance/getprices?i=#{interval}&p=#{period}&f=d,c&df=cpct&auto=0&q=#{params[:q]}"
-
-          RestClient::Request.execute(url: url, method: :get, verify_ssl: false) do |response|
-            raw = response.body.split[8..-1] || []
-            csv = CSV.new(raw.join("\n"))
-            data = csv.to_a.map do |row|
-              next unless row.count == 2
-              row[1].to_f
-            end.compact
-
-            g = Gruff::Line.new
-            g.data params[:q], data
-            content_type 'image/png'
-            g.to_blob
-          end
+          chart = IEX::Resources::Chart.get(params[:q], period)
+          data = chart.map(&:high)
+          g = Gruff::Line.new
+          g.data params[:q], data
+          content_type 'image/png'
+          g.to_blob
         end
       end
     end

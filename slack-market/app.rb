462a0c089c64745657fa1ff1922529c43de153ec
dblock <dblock@dblock.org>
2018-08-22 08:34:01 -0400

    Upgraded slack-ruby-bot-server 0.7.0.

--- slack-market/app.rb
+++ slack-market/app.rb
@@ -7,9 +7,6 @@ module SlackMarket
         check_subscribed_teams!
         deactivate_asleep_teams!
       end
-      once_and_every 60 * 3 do
-        ping_teams!
-      end
     end
 
     private
@@ -18,25 +15,6 @@ module SlackMarket
       yield
       every tt do
         yield
-      end
-    end
-
-    def ping_teams!
-      Team.active.each do |team|
-        begin
-          ping = team.ping!
-          next if ping[:presence].online
-          logger.warn "DOWN: #{team}"
-          after 60 do
-            ping = team.ping!
-            unless ping[:presence].online
-              logger.info "RESTART: #{team}"
-              SlackMarket::Service.instance.start!(team)
-            end
-          end
-        rescue StandardError => e
-          logger.warn "Error pinging team #{team}, #{e.message}."
-        end
       end
     end
 

de2b32d67af98c57d58d51e3eb2a86c62748d8d7
dblock <dblock@dblock.org>
2019-06-25 19:48:56 -0400

    Disable, too expensive.

--- slack-market/api/endpoints/charts_endpoint.rb
+++ slack-market/api/endpoints/charts_endpoint.rb
@@ -10,19 +10,20 @@ module Api
         desc 'Get chart.'
         params do
           requires :q, type: String
-          optional :i, type: Integer, default: 360
           optional :p, type: String, default: '1d'
         end
         get ':q' do
+          error!('Currently Disabled', 400)
+
           expire_in = case params[:p]
                       when '1d' then 60 * 60
                       else 60 * 60 * 12
                       end
+
           header 'Expires', CGI.rfc1123_date(Time.now.utc + expire_in)
-          key = "api:charts:#{params[:q]}:#{params[:i]}:#{params[:p]}"
-          Api::Middleware.logger.info(key)
+          key = "api:charts:#{params[:q]}}:#{params[:p]}"
+
           Cachy.cache(key, expires_in: expire_in) do
-            Api::Middleware.logger.info("#{key}: cache miss")
             client = IEX::Api::Client.new
             chart = client.chart(params[:q], params[:p])
             data = chart.map(&:high).select { |v| v && v >= 0 }

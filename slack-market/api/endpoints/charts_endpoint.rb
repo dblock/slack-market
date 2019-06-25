280aa11073a015122f6996fc345433b7582a3829
dblock <dblock@dblock.org>
2019-06-25 19:43:27 -0400

    Cache charts more aggressively for all but intraday.

--- slack-market/api/endpoints/charts_endpoint.rb
+++ slack-market/api/endpoints/charts_endpoint.rb
@@ -14,7 +14,10 @@ module Api
           optional :p, type: String, default: '1d'
         end
         get ':q' do
-          expire_in = 60 * 60 * 1
+          expire_in = case params[:p]
+                      when '1d' then 60 * 60
+                      else 60 * 60 * 12
+                      end
           header 'Expires', CGI.rfc1123_date(Time.now.utc + expire_in)
           key = "api:charts:#{params[:q]}:#{params[:i]}:#{params[:p]}"
           Api::Middleware.logger.info(key)

a7cbbee0551578dcd3f58741aaec4854fade5e14
dblock <dblock@dblock.org>
2020-06-26 20:50:17 -0400

    Remove remaining GIFs.

--- slack-market/commands/set.rb
+++ slack-market/commands/set.rb
@@ -5,22 +5,22 @@ module SlackMarket
 
       subscribe_command 'set' do |client, data, match|
         if !match['expression']
-          client.say(channel: data.channel, text: 'Missing setting, eg. _set dollars on_.', gif: 'help')
+          client.say(channel: data.channel, text: 'Missing setting, eg. _set dollars on_.')
           logger.info "SET: #{client.owner} - failed, missing setting"
         else
           k, v = match['expression'].split(/\W+/, 2)
           case k
           when 'charts' then
             client.owner.update_attributes!(charts: v.to_b) unless v.nil?
-            client.say(channel: data.channel, text: "Charts for team #{client.owner.name} are #{client.owner.charts? ? 'on!' : 'off.'}", gif: 'charts')
+            client.say(channel: data.channel, text: "Charts for team #{client.owner.name} are #{client.owner.charts? ? 'on!' : 'off.'}")
             logger.info "SET: #{client.owner} - charts are #{client.owner.charts? ? 'on' : 'off'}"
           when 'dollars' then
             client.owner.update_attributes!(dollars: v.to_b) unless v.nil?
-            client.say(channel: data.channel, text: "Dollar signs for team #{client.owner.name} are #{client.owner.dollars? ? 'on!' : 'off.'}", gif: 'dollars')
+            client.say(channel: data.channel, text: "Dollar signs for team #{client.owner.name} are #{client.owner.dollars? ? 'on!' : 'off.'}")
             logger.info "SET: #{client.owner} - dollar signs are #{client.owner.dollars? ? 'on' : 'off'}"
           when 'bots' then
             client.owner.update_attributes!(bots: v.to_b) unless v.nil?
-            client.say(channel: data.channel, text: "Bots for team #{client.owner.name} are #{client.owner.bots? ? 'allowed!' : 'not allowed.'}", gif: 'bots')
+            client.say(channel: data.channel, text: "Bots for team #{client.owner.name} are #{client.owner.bots? ? 'allowed!' : 'not allowed.'}")
             logger.info "SET: #{client.owner} - bots are #{client.owner.bots? ? 'on' : 'off'}"
           else
             raise "Invalid setting #{k}, you can _set dollars on|off_."
